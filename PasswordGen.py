8844ca094a14970c0cb9072f56ab51985775f70a
Cosgun <60076142+MisterCxmpy@users.noreply.github.com>
2020-01-20 16:26:46 +0000

    Add files via upload

---
+++ PasswordGen.py
@@ -0,0 +1,23 @@
+#Basic Password Gen Made With Python
+
+import random
+import sys
+import time
+import os
+
+char = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!£$%^&*"
+
+print("Basic password generator made with Python")
+time.sleep(0.5)
+password_length = int(input("Input a password length: "))
+site = input("Input the site: ")
+
+password = ""
+for l in range(password_length):
+    password += random.choice(char)
+currentDirectory = os.getcwd()
+print("Your password has been saved under", currentDirectory, "- password.txt")
+file = open("password.txt","a")
+file.write(password + ' | ' + site)
+file.write("\n")
+file.close()
