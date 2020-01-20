282989356b991249c37245eed71a553768f3df22
Cosgun <60076142+MisterCxmpy@users.noreply.github.com>
2020-01-20 13:49:35 +0000

    Add files via upload

---
+++ calend.py
@@ -0,0 +1,24 @@
+import calendar
+import time
+
+
+def cal():
+    year = int(input("Enter a year: "))
+    
+    def month_():
+        month = int(input("Enter a month: "))
+        print("\n")
+        if month > 12:
+            print("\nInvalid month\n")
+            month_()
+        else:
+            c = calendar.TextCalendar(calendar.MONDAY)
+            str = c.formatmonth(year, month)
+            print(str)
+            time.sleep(2)
+            cal()
+
+
+    month_()
+
+cal()
